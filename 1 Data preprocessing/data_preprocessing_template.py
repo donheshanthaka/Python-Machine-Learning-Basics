e673eba3140a65c4e7c57692b1fdad2db33b07ad
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-01 22:50:50 +0530

    Importing the libraries

--- 1 Data preprocessing/data_preprocessing_template.py
+++ 1 Data preprocessing/data_preprocessing_template.py
@@ -1,15 +1,10 @@
-# Data Preprocessing Template
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 22:44:32 2022
 
-# Importing the libraries
+@author: Don Hesha
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
