89ff50b10bc428e1f69ac939f7fd107837bb4af8
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-02 03:06:45 +0530

    splitting dataset into training and test set

--- 1 Data preprocessing/data_preprocessing_template.py
+++ 1 Data preprocessing/data_preprocessing_template.py
@@ -18,7 +18,7 @@ dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, :-1].values
 
 # Dependent variable vector
-Y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
@@ -39,5 +39,33 @@ ct = ColumnTransformer([("Country", OneHotEncoder(), [0])], remainder = 'passthr
 X = ct.fit_transform(X)
 
 # Dependent column
-labelEncoder_Y = LabelEncoder()
-Y = labelEncoder_Y.fit_transform(Y)
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+
+# Splitting the dataset into the Training and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
