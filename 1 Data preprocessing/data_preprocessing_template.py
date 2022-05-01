a9d33b697082b3d6a64af88bdf37ef70310cabf3
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-01 23:04:17 +0530

    Importing the dataset

--- 1 Data preprocessing/data_preprocessing_template.py
+++ 1 Data preprocessing/data_preprocessing_template.py
@@ -5,6 +5,17 @@ Created on Sun May  1 22:44:32 2022
 @author: Don Hesha
 """
 
+# Libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Dataset
+dataset = pd.read_csv("Data.csv")
+
+# First take all the rows and then take all the columns except the last
+# Aka independent variable vector
+X = dataset.iloc[:, :-1].values
+
+# Dependent variable vector
+Y = dataset.iloc[:, 3].values
