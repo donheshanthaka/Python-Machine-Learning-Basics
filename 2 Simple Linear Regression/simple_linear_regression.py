246fcb3f99361107b105a92a3782392b41aa9d5e
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-02 23:10:46 +0530

    Importing the dataset and splitting the test and train sets

--- 2 Simple Linear Regression/simple_linear_regression.py
+++ 2 Simple Linear Regression/simple_linear_regression.py
@@ -5,3 +5,35 @@ Created on Mon May  2 22:13:23 2022
 @author: Don Hesha
 """
 
+# Simple Linear Regression
+
+
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Dataset
+dataset = pd.read_csv("Salary_Data.csv")
+
+# First take all the rows and then take all the columns except the last
+# aka independent variable matrix
+X = dataset.iloc[:, :-1].values
+
+# Dependent variable vector
+y = dataset.iloc[:, 1].values
+
+
+# Splitting the dataset into the Training and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+
+
+"""
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+"""
