75ce7b27d4babc82e908665dc86ebee655547a54
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-03 07:15:49 +0530

    Visualized the train and test set results

--- 2 Simple Linear Regression/simple_linear_regression.py
+++ 2 Simple Linear Regression/simple_linear_regression.py
@@ -48,7 +48,22 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title("Salary vs Experience (Training set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
 
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title("Salary vs Experience (Test set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
 
 
 
