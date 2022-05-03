30eae53b3cf034e11dee92af0726917f8eb2cd25
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-03 21:45:26 +0530

    Fitting the multiple linear regression to the training set

--- 3. Multiple Linear Regression/multiple_linear_regression.py
+++ 3. Multiple Linear Regression/multiple_linear_regression.py
@@ -42,12 +42,38 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
+# Fitting multiple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
 
-"""
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
