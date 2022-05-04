3f4ca74198c469095669ea06e8497f1ee21fcabc
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-04 07:44:56 +0530

    Visualizing the polynomial regression results

--- 4. Polynomial Regression/polynomial_regression.py
+++ 4. Polynomial Regression/polynomial_regression.py
@@ -34,12 +34,13 @@ from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
+
 # Fitting polynomial regression to the dataset
 
 # Converted the original matrix of X to new maxtrix of features containing the original independent position
 # ... levels and its associated polynomial terms
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
 
 # Creation of a new linear regression model with new X_poly matrix
@@ -58,6 +59,25 @@ plt.show()
 
 
 # Visualzing the polynomial regression results
+
+# Create a vector of X with more resolution
+X_grid = np.arange(min(X), max(X), 0.1)
+
+# Reshaping the newly created vector to a matirx
+X_grid = X_grid.reshape((len(X_grid), 1))
+
+plt.scatter(X, y, color = "red")
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = "blue")
+plt.title("Truth or Bluff (Polynomial Regression)")
+plt.xlabel("Position level")
+plt.ylabel("Salary")
+plt.show()
+
+
+
+
+
+
 
 
 
