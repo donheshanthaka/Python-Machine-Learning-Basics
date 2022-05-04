6d0f4282a7f70f1c37dd19775635756e9239ad4c
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-05 03:56:44 +0530

    Fitting the decision tree model

---
+++ 6. Decision Tree Regression/decision_tree_regression.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 03:46:28 2022
+
+@author: Don Hesha
+"""
+
+# Decision Tree Regression
+
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Dataset
+dataset = pd.read_csv("Position_Salaries.csv")
+
+# First take all the rows and then take all the columns except the last
+# Aka independent variable vector
+# By adding the :2 after 1 will make the vector a matrix which is much suitable for this kind of computation
+X = dataset.iloc[:, 1:2].values
+
+# Dependent variable vector
+y = dataset.iloc[:, 2].values
+
+
+# No dataset splitting since a small number of samples are used
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+"""
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+y_train = sc_y.fit_transform(y_train)
+"""
+
+
+# Fitting the Decision Tree Regression model to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+
+# Predicting a new result
+y_pred = regressor.predict(np.array([6.5]).reshape(1, 1))
+y_pred = regressor.predict([[6.5]])
+
+
+# Visualizing the Decision Tree Regression results
+
+plt.scatter(X, y, color = "red")
+plt.plot(X, regressor.predict(X), color = "blue")
+plt.title("Truth or Bluff (Decision Tree Regression Model)")
+plt.xlabel("Position level")
+plt.ylabel("Salary")
+plt.show()
