b7847257db71a8a548010e43201987572e903d0e
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-08 06:25:37 +0530

    Making the confusion matrix

--- 8. Logistic Regression/logistic_regression.py
+++ 8. Logistic Regression/logistic_regression.py
@@ -46,7 +46,9 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
 
-
+# Making a confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 
 
 
