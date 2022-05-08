e7452201ff292275cb74b04a105ad83b6769ba0a
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-08 06:11:09 +0530

    Splitting the dataset and adding feature scaling

--- 8. Logistic Regression/logistic_regression.py
+++ 8. Logistic Regression/logistic_regression.py
@@ -24,16 +24,12 @@ y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 
 
-"""
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-sc_y = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-y_train = sc_y.fit_transform(y_train)
-"""
