dc3551afb1f7e3b82b662176f4c82aab802504f0
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-08 06:05:16 +0530

    Importing the dataset

--- 8. Logistic Regression/logistic_regression.py
+++ 8. Logistic Regression/logistic_regression.py
@@ -5,3 +5,35 @@ Created on Sun May  8 05:54:37 2022
 @author: Don Hesha
 """
 
+
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Dataset
+dataset = pd.read_csv("Social_Network_Ads.csv")
+
+# First take all the rows and then take all the columns except the last
+# Aka independent variable vector
+X = dataset.iloc[:, [2,3]].values
+
+# Dependent variable vector
+y = dataset.iloc[:, 4].values
+
+
+# Splitting the dataset into the Training and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+
+"""
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+y_train = sc_y.fit_transform(y_train)
+"""
