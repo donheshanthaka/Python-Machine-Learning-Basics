81afb4500bec6846693c2be5e588836f7e6bd9a2
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-08 06:41:26 +0530

    Visualizing the training and test set results

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,55 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May  8 05:54:37 2022
+# Logistic Regression
 
-@author: Don Hesha
-"""
-
-# Logistic Regression 
-
-# Libraries
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Dataset
-dataset = pd.read_csv("Social_Network_Ads.csv")
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, -1].values
 
-# First take all the rows and then take all the columns except the last
-# Aka independent variable vector
-X = dataset.iloc[:, [2,3]].values
-
-# Dependent variable vector
-y = dataset.iloc[:, 4].values
-
-
-# Splitting the dataset into the Training and Test set
+# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-
-
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
-
-# Fitting the logistic regression to the training set
+# Training the Logistic Regression model on the Training set
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
-
-# Predicting the test set results
+# Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-
-# Making a confusion matrix
+# Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
+print(cm)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
@@ -69,7 +51,6 @@ plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
-
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
@@ -87,14 +68,3 @@ plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
