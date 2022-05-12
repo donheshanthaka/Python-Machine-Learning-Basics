dbddcf8c7f248bb13e58cb9fb46f990216af6981
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-12 19:48:55 +0530

    Fitting the random forest classification to the training set

--- 14. Random Forest Classification/random_forest_classification.py
+++ 14. Random Forest Classification/random_forest_classification.py
@@ -37,7 +37,9 @@ X_test = sc_X.transform(X_test)
 
 
 # Fitting the Random Forest Classification to the training set
-# Create the classifier here
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = "entropy", random_state = 0)
+classifier.fit(X_train, y_train)
 
 
 # Predicting the test set results
