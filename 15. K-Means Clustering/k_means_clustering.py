996b9f402d5d5f0c7c59f65b7713ecadb39ad022
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-13 20:04:45 +0530

    Applying k-means to the mall dataset

--- 15. K-Means Clustering/k_means_clustering.py
+++ 15. K-Means Clustering/k_means_clustering.py
@@ -27,3 +27,10 @@ plt.title("The Elbow Method")
 plt.xlabel("Number of clusters")
 plt.ylabel("WCSS")
 plt.show()
+
+
+# Applying K-means to the mall dataset
+kmeans = KMeans(n_clusters = 5, init = "k-means++", max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
+
+
