04d5d0f87a7c431041f6073f1b0b1e77af15dc41
Don Heshanthaka <heshanthaka007@gmail.com>
2022-05-13 20:00:51 +0530

    Using the elbow method to find the optimal number of clusters

--- 15. K-Means Clustering/k_means_clustering.py
+++ 15. K-Means Clustering/k_means_clustering.py
@@ -5,3 +5,25 @@ Created on Fri May 13 19:42:40 2022
 @author: Don Hesha
 """
 
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the mall dataset
+dataset = pd.read_csv("Mall_Customers.csv")
+X = dataset.iloc[:, [3,4]].values
+
+# Using the elbow the method to find the optimal number of customers
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = "k-means++", max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+    
+plt.plot(range(1, 11), wcss)
+plt.title("The Elbow Method")
+plt.xlabel("Number of clusters")
+plt.ylabel("WCSS")
+plt.show()
